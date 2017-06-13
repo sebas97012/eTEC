17dc401aca092b94dad966ee0f777a5db8425a00
sebas97012 <sebas.smr@hotmail.com>
2017-06-13 13:46:25 -0600

    Implementación de páginas básicas con PM

---
+++ src/org/tec/ce/eTEC/beans/IndexBean.java
@@ -0,0 +1,13 @@
+package org.tec.ce.eTEC.beans;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ * Created by sebas97012 on 6/13/17.
+ */
+@ManagedBean(name="index")
+public class IndexBean {
+    public String prueba(){
+        return "Administrador";
+    }
+}
